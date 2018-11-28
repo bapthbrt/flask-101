ee5c18f9d4aeb0feafa3b613db3a084fe0acc791
Baptiste Hebert <baptiste.hebert@gmail.com>
2018-11-28 11:32:09 +0100

    Read product

--- wsgi.py
+++ wsgi.py
@@ -4,6 +4,15 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+PRODUCTS = [
+        {
+            'id': 1, 'name': 'Skello'
+        }, {
+            'id': 2, 'name': 'Socialive.tv'
+        }, {
+            'id': 3, 'name': 'Test'
+        }
+    ]
 
 @app.route('/')
 def hello():
@@ -12,12 +21,12 @@ def hello():
 
 @app.route('/api/v1/products')
 def get_products():
-    return jsonify([
-        {
-            'id': 1, 'name': 'Skello'
-        }, {
-            'id': 2, 'name': 'Socialive.tv'
-        }, {
-            'id': 3, 'name': 'Test'
-        }
-    ])
+    return jsonify(PRODUCTS)
+
+
+@app.route('/api/v1/products/<int:id>')
+def get_product(id):
+    for i in range(len(PRODUCTS)):
+        if PRODUCTS[i].get('id') == id:
+            return jsonify(PRODUCTS[i])
+    return '', 404
